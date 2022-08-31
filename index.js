41cfe3b5db5494f64bed4edd63be5c8118dc23d8
Kiwg <gkiwure21@gmail.com>
2022-08-31 12:56:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+for (let i = 0; i < word.length / 2; i++){
+  const f = word.length - 1 - i;
+  if(word[i] !==word[f]) {
+    return false
+  }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+return true;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
